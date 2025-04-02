a70aa64a3a62fbb63d0680c511744f361bfd5020
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2025-04-02 12:35:55 +0530

    Delete customerManagement directory

--- customerManagement/src/main/java/com/example/transactionManagement/exceptions/InvalidTransactionException.java
+++
@@ -1,7 +0,0 @@
-package com.example.transactionManagement.exceptions;
-
-public class InvalidTransactionException extends RuntimeException {
-    public InvalidTransactionException(String message) {
-        super(message);
-    }
-}
--- customerManagement/src/main/java/com/example/transactionManagement/exceptions/NoTransactionsFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.transactionManagement.exceptions;
-
-public class NoTransactionsFoundException extends RuntimeException {
-    public NoTransactionsFoundException(String message) {
-        super(message);
-    }
-}
